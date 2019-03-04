bf1b48c80da4e4c595424fecd98985642d66bd2c
lubcountcooper <lub@countcooper.nl>
2019-03-04 10:01:57 +0800

    cleaning up code

--- week_02/mini_projects/04_guess_analyse.py
+++ week_02/mini_projects/04_guess_analyse.py
@@ -108,17 +108,11 @@ print(f"Average turns Strategy 2: {average_turns2}")
 
 
 # Overlay 2 histograms to compare them
-def overlaid_histogram(data1, data2, n_bins = 0, data1_name="", data1_color="#539caf", data2_name="", data2_color="#7663b0",x_label="", y_label="", title=""):
+def overlaid_histogram(data1, data2, data1_name="", data1_color="#539caf", data2_name="", data2_color="#7663b0",x_label="", y_label="", title=""):
     # Set the bounds for the bins so that the two distributions are fairly compared
     max_nbins = 10
     data_range = [min(min(data1), min(data2)), max(max(data1), max(data2))]
     binwidth = (data_range[1] - data_range[0]) / max_nbins
-
-
-    if n_bins == 0:
-    	bins = np.arange(data_range[0], data_range[1] + binwidth, binwidth)
-    else:
-    	bins = n_bins
 
     # Create the plot
     _, ax = plt.subplots()
