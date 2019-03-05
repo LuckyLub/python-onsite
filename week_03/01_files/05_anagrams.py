d88db77211e63be295b680812d33a6431f84e5ed
lubcountcooper <lub@countcooper.nl>
2019-03-05 16:14:58 +0800

    05_anagrams done

--- week_03/01_files/05_anagrams.py
+++ week_03/01_files/05_anagrams.py
@@ -15,3 +15,24 @@ Source: Read through the "Files" chapter in Think Python 2e:
 http://greenteapress.com/thinkpython2/html/thinkpython2015.html
 
 '''
+
+import anagram_creator
+
+def store_anagrams(anagram_dict, shelf):
+    shelf.append(anagram_dict)
+
+def read_anagrams(word, anagram_dict):
+    for v in anagram_dict.values():
+        for item in v:
+            if item == word:
+                return v
+
+
+shelf1 = []
+anagram_map = anagram_creator.all_anagrams('words.txt')
+anagram_creator.print_anagram_sets_in_order(anagram_map)
+store_anagrams(anagram_map, shelf1)
+print(read_anagrams("aah",anagram_map))
+
+# eight_letters = anagram_creator.filter_length(anagram_map, 8)
+# anagram_creator.print_anagram_sets_in_order(eight_letters)
